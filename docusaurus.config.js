2db1df8afeaef0c76ebd1c626bf608eb5185e103
CubityFirst <cubity@cubityfir.st>
2024-06-28 18:18:19 +0100

    remove blog temporarily

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -78,7 +78,7 @@ const config = {
             position: 'left',
             label: 'Guides',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          //{to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/cubityfirst/skyblockguides',
             label: 'GitHub',
